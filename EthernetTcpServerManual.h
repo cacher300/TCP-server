86387d394a597558b45256afb95b44d93ec4e2b5
cacher300 <77995433+cacher300@users.noreply.github.com>
2024-09-16 09:40:02 -0400

    Update EthernetTcpServerManual.h

--- EthernetTcpServerManual.h
+++ EthernetTcpServerManual.h
@@ -4,7 +4,6 @@
 #include "ClearCore.h"
 #include "EthernetTcpServer.h"
 
-// Configuration and global variables
 #define PORT_NUM 8888
 #define MAX_PACKET_LENGTH 100
 #define NUMBER_OF_CLIENTS 6
@@ -14,9 +13,8 @@ extern unsigned char packetReceived[MAX_PACKET_LENGTH];
 extern char packetToSend[MAX_PACKET_LENGTH];
 extern Connector *const outputLEDs[6];
 
-// Function declarations
 void InitServer();
 void ManageClients();
 bool GetClientMessage(char *messageBuffer, int bufferLength);
 
-#endif // ETHERNETTCPSERVERMANUAL_H
+#endif 
